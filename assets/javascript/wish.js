472dd4b83b4e898e30bae598ba8d1811dd052f0a
unknown <whoisdai2003@gmail.com>
2021-12-24 07:14:15 +0700

    ok

---
+++ assets/javascript/wish.js
@@ -0,0 +1,20 @@
+
+ let menutoggle = document.querySelector('.toggle');
+let menuopen = document.querySelector('.menu')
+menutoggle.onclick = function(){
+  menutoggle.classList.toggle('active')
+  menuopen.classList.toggle('active')
+} 
+
+
+
+/* let spining =document.querySelector('.spin-button');
+let startspin =document.querySelector('.inner-wheel');
+
+spining.onclick =function(){
+
+spining.classList.toggle('actived')
+startspin.classList.toggle('actived')
+}
+ */
+
